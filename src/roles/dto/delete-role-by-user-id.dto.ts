4376dcdb56554ba6fe94b8ce41d91e592c9d3636
JoystickNS <deniska-97@list.ru>
2022-04-27 19:16:16 +0300

    changed type of role

--- src/roles/dto/delete-role-by-user-id.dto.ts
+++ src/roles/dto/delete-role-by-user-id.dto.ts
@@ -1,10 +1,9 @@
-import { RoleEnum } from "@prisma/client";
-import { IsEnum, IsInt } from "class-validator";
+import { IsInt } from "class-validator";
 
 export class DeleteRoleByUserIdDto {
   @IsInt()
   userId: number;
 
-  @IsEnum(RoleEnum)
-  role: RoleEnum;
+  @IsInt()
+  roleId: number;
 }
