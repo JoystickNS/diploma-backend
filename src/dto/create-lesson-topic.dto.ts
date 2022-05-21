10b69513ae49da092b8e44412c77861eb5561a06
JoystickNS <deniska-97@list.ru>
2022-05-21 17:38:30 +0300

    deleted numberOfHours

--- src/dto/create-lesson-topic.dto.ts
+++ src/dto/create-lesson-topic.dto.ts
@@ -1,13 +1,8 @@
-import { IsInt, IsOptional, IsString, MaxLength, Min } from "class-validator";
+import { IsOptional, IsString, MaxLength } from "class-validator";
 
 export class CreateLessonTopicDto {
   @IsOptional()
   @IsString()
   @MaxLength(200)
-  readonly name?: string;
-
-  @IsOptional()
-  @IsInt()
-  @Min(0)
-  readonly numberOfHours?: number;
+  readonly name: string;
 }
